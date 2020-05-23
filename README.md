cdad9c9a00df186f1f61f0125b53ae15f504ef49
Carlos Eduardo <carlosrduardozero0@gmail.com>
2020-05-23 10:59:18 -0300

    consertando o repositorio

--- README.md
+++
@@ -1,2 +0,0 @@
-# omnistack-mobile
-atividade 
--- src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'https://omnistack-levissa.herokuapp.com/'
-});
-
-export default api;
